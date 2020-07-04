5dff74a4b2f2f714ab6338fd9a0f9832aa0d6277
Junghyun Gan <jhgan00@gmail.com>
2020-07-04 23:19:40 +0900

    Delete ckpt manager

--- modules.py
+++ modules.py
@@ -63,5 +63,3 @@ checkpoint = tf.train.Checkpoint(
     generator=generator,
     discriminator=discriminator
 )
-
-checkpoint_manager = tf.train.CheckpointManager(checkpoint, "./training_checkpoints")

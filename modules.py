5cbd4eeaef8e8d8f44780cddb1a537a1d95b175e
Junghyun Gan <jhgan00@gmail.com>
2020-07-05 00:57:57 +0900

    Update loss objs

--- modules.py
+++ modules.py
@@ -49,18 +49,15 @@ discriminator_optimizer = Adam(1e-4)
 
 
 # Losses
-disc_cross_entropy = BinaryCrossentropy(from_logits=True)
-disc_loss = Sum("Loss(D)")
-gen_loss = BinaryCrossentropy(from_logits=True, name="Loss(G)")
+cross_entropy = BinaryCrossentropy(from_logits=True)
 
 def discriminator_loss(real_output, fake_output):
-    real_loss = disc_cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = disc_cross_entropy(tf.zeros_like(fake_output), fake_output)
-    total_loss = disc_loss(real_loss + fake_loss)
-    return total_loss
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    return real_loss + fake_loss
 
 def generator_loss(fake_output):
-    return gen_loss(tf.ones_like(fake_output), fake_output)
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 checkpoint = tf.train.Checkpoint(
     generator_optimizer=generator_optimizer,
